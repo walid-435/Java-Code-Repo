3b24c4d42dbdd34075e072409c9e47d4f21fb985
Walid Rahman <rahman241-35-435@diu.edu.bd>
2025-04-06 11:37:19 +0600

    Delete LabTasks directory

--- LabTasks/Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Input {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        String name=sc.nextLine();
-        System.out.println(name);
-    }
-    
-}
